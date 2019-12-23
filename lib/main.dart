f3f8301da364e786c5136710e41ff5830295ecd1
gobutaka <takaaki.hirakura@game8.jp>
2019-12-23 14:42:25 +0900

    create screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc_sample/blocs/counter_bloc.dart';
+import 'package:flutter_bloc_sample/screens/counter_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -25,54 +26,8 @@ class MyApp extends StatelessWidget {
         */
         dispose: (context, bloc) => bloc.dispose(),
         // widgetを渡す
-        child: MyHomePage(title: 'Flutter Demo Home Page'),
+        child: CounterScreen(title: 'Flutter Demo Home Page'),
       )
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage({this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    // blocは子widgetのbuild()メソッドで呼ぶのが定番
-    final counterBloc = Provider.of<CounterBloc>(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            // StreamBuilderを使うことで、この箇所だけUIを更新することができる
-            StreamBuilder(
-              initialData: 0,
-              stream: counterBloc.count,
-              builder: (context, snapshot) {
-                return Text(
-                  '${snapshot.data}',
-                  style: Theme.of(context).textTheme.display1,
-                );
-              },
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // カウントアップアクションをblocに送る
-          counterBloc.increment.add(null);
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
---
+++ lib/screens/counter_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter_bloc_sample/blocs/counter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CounterScreen extends StatelessWidget {
+  CounterScreen({this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    // blocは子widgetのbuild()メソッドで呼ぶのが定番
+    final counterBloc = Provider.of<CounterBloc>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            // StreamBuilderを使うことで、この箇所だけUIを更新することができる
+            StreamBuilder(
+              initialData: 0,
+              stream: counterBloc.count,
+              builder: (context, snapshot) {
+                return Text(
+                  '${snapshot.data}',
+                  style: Theme.of(context).textTheme.display1,
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // カウントアップアクションをblocに送る
+          counterBloc.increment.add(null);
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
